40fe245e1b3ad8aae9615363d5be9b6800f3ff78
Tejesh Shrestha <tejesh.shrestha@deerwalk.edu.np>
2023-05-29 08:25:10 +0545

    string

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      HEllo {props.name}, you are {props.age} years old!
+      <br />
+    </>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const name = "Tej";
+  const age = 20;
+  const friends = [
+    { name: "Ram", age: 90 },
+    { name: "Sita", age: 78 },
+  ];
+  return (
+    <div>
+      <p>Hello World, it is {now.toString()}</p>
+      <br />
+      <Hello name="Tejesh" age={10 + 90} />
+      <Hello name={name} age={age} />
+      <p>{friends[1].name}</p>
+      <p>{friends[0].age}</p>
     </div>
   );
-}
+};
 
 export default App;
